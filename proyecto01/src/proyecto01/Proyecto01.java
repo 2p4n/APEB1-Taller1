4608d6de52db2504c895e8413f7892716781c4e5
Juan Diego <jdguerrero021422@gmail.com>
2023-10-22 12:28:56 -0500

    primer commit

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,8 @@
+package proyecto01;
+public class Proyecto01 {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo basico de Java versionado a traves de Git manejado desde GitHub");
+                
+    }
+    
+}
